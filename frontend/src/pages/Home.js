18ddf19c1e8747b5d548f7b163a90b75c06cd066
manimozhiyan23 <manimozhiyan88@gmail.com>
2025-08-11 21:14:43 +0530

    starting

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,18 @@
+import React from "react";
+import '../styles/home.css';
+
+function Home() {
+  return (
+    <>
+    <div className="whole-page">
+        <div className="mani">
+            <p>Mani</p>
+        </div>
+
+    </div>
+    </>
+
+  );
+}
+
+export default Home;
